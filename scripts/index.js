d57178539c5d2aaac39fa3401d9daf7a3d0fcec1
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2023-01-31 11:59:29 +0100

    Jupla :rocket:

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Hello world!");
+
+    const text = document.querySelector(".welcome");
+    text ? text.innerHTML = "Hello world!" : "";
+});
