a1f26bbb167e1b6672ec694f1886f1bcbefe5161
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2023-01-31 16:13:13 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello world!");
+    console.log("Hello world!!");
 
     const text = document.querySelector(".welcome");
     text ? text.innerHTML = "Hello world!" : "";
